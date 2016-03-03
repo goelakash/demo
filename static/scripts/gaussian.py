2607a32e4bbbc6497be852c0effa15ae3a4d71c0
goelakash <goelakash93@gmail.com>
2016-03-03 15:59:46 +0530

    File I/O working

--- static/scripts/gaussian.py
+++ static/scripts/gaussian.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 import cv2
 import numpy as np
-import sys
 
 # img = cv2.imread('leena.jpeg')
 img = cv2.imread(sys.argv[1])
-blur = cv2.blur(img,(5,5))
-return blur
+blur = cv2.GaussianBlur(img,(7,7),0)
+cv2.imwrite(sys.argv[1],blur)
+
+# return blur
 # cv2.imshow('Blur',blur)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()        # Closes displayed windows
--- upload.py
+++ upload.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import FileField, SubmitField, SelectField, ValidationError
+import cv2
+from subprocess import call
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top secret!'
@@ -15,7 +17,7 @@ lst=[]
 
 def fill_list():
     count = 0
-    for file in os.listdir(os.getcwd()+'/scripts'):
+    for file in os.listdir(os.path.join(app.static_folder, "scripts")):
         if file[-2:]=='py':
             lst.append((count,file[:-3]))
             count+=1
@@ -35,20 +37,26 @@ pipeline = []
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global image,pipeline
+
     uform = UploadForm()
     pform = PipelineForm()
 
     if pform.validate_on_submit():
         operation = pform.select_op.data
-        print lst[operation][1]
-        pipeline.append(lst[operation][1])
+        op = lst[operation][1]
+        pipeline.append(op)
 
-    elif uform.validate_on_submit():
-        pipeline = None
+        file_location = os.path.join(app.static_folder, image)
+        script_location = os.path.join(app.static_folder, "scripts/"+pipeline[-1]+".py")
+        print file_location, script_location
+        call(["python",script_location,file_location])
+
+
+    if uform.validate_on_submit():
         if hasattr(uform.image_file.data,'filename'):
+            pipeline = []
             image = 'temp/' + uform.image_file.data.filename
             uform.image_file.data.save(os.path.join(app.static_folder, image))
-
 
     return render_template('index.html', uform=uform, pform=pform, image=image, pipeline = pipeline)
 
