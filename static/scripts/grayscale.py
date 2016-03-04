ebd162751ca728e779216a410aa67f016031dd66
goelakash <goelakash93@gmail.com>
2016-03-05 02:23:54 +0530

    sorted out frontend

--- static/scripts/grayscale.py
+++ static/scripts/grayscale.py
@@ -5,7 +5,7 @@ import sys
 # img = cv2.imread('leena.jpeg')
 img = cv2.imread(sys.argv[1])
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite(sys.argv[1],gray_image)
+cv2.imwrite(sys.argv[2],gray_image)
 # cv2.imshow('gray_image',gray_image)
 # cv2.waitKey(0)                 # Waits forever for user to press any key
 # cv2.destroyAllWindows()        # Closes displayed windows
--- upload.py
+++ upload.py
@@ -1,13 +1,16 @@
 import os
+import time
 import imghdr
 from flask import Flask, render_template, make_response, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
-from wtforms import FileField, SubmitField, SelectField, ValidationError, widgets
+# from flask_pymongo import PyMongo
+from wtforms import FileField, TextField, SubmitField, SelectField, ValidationError, widgets
 from wtforms.validators import Required
 from subprocess import call
 
 app = Flask(__name__)
+# mongo = PyMongo(app)
 app.config['SECRET_KEY'] = 'top secret!'
 bootstrap = Bootstrap(app)
 
@@ -21,49 +24,92 @@ def fill_list():
             count+=1
 
 class PipelineForm(Form):
-    select_op = SelectField('Apply Operation',coerce=int,choices=lst)
-    submit = SubmitField('Submit')
-    lsit = widgets.ListWidget()
-
+    width = TextField('Width')
+    height = TextField('Height')
+    submit = SubmitField('Final Submit')
 
 class UploadForm(Form):
-    image_file = FileField('Image file',validators=[	])
+    image_file = FileField('Image file')
     submit = SubmitField('Submit')
 
 image = None
-pipeline = []
+tuple_list = []
+
+@app.route('/apply', methods=['GET', 'POST'])
+def apply():
+    if pform.validate_on_submit():
+        global tuple_list,image
+        pipeline_1 = []
+        #get the pipeline operations
+        for a,b in tuple_list:
+            pipeline_1.append(a)
+
+        val = request.files['file']
+        #conver val to pipeline_2
+
+
+        if pipeline_1 != pipeline_2:
+            i = 0;
+            min_len = min(len(pipeline_1),len(pipeline_2))
+            while i<min_len:
+                if pipeline_1[i]!=pipeline_2[i]:
+                    break
+                i=i+1
+
+            if i<len(pipeline_1):
+                tuple_list = tuple_list[:i]
+
+            while i<len(pipeline_2):
+                op = lst[pipeline_2[i]]
+
+                input_location = os.path.join(app.static_folder, tuple_list[i-1][1])
+                script_location = os.path.join(app.static_folder, "scripts/"+op+".py")
+
+                print input_location, script_location
+
+                ext = input_location[input_location.rfind('.')+1:]
+                timestamp = int(time.time())
+                output_location = input_location[:input_location.index('_')+1]+str(timestamp)+"."+ext
+
+                print output_location
+
+                call(["python",script_location,input_location,output_location])
+                #get image from db
+                #do the operation of pipeline_2[i]
+                #store the image at (image_ID with i) location
+
+                # tuple_list.append((pipeline_2[i]),new_img)
+
+                i=i+1
+            image = tuple_list[-1][1]
+
+
+        # if image != None:
+            # file_location = os.path.join(app.static_folder, image)
+            # script_location = os.path.join(app.static_folder, "scripts/"+pipeline[-1]+".py")
+            # print file_location, script_location
+            # call(["python",script_location,file_location])
+        # else:
+        #     pipeline_1=pipeline_2=[]
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global image,pipeline
+    global image,tuple_list
     # pipeline = []
 
     uform = UploadForm()
     pform = PipelineForm()
 
-    if pform.validate_on_submit():
-        operation = pform.select_op.data
-        op = lst[operation][1]
-        pipeline.append(op)
-
-        if image != None:
-            file_location = os.path.join(app.static_folder, image)
-            script_location = os.path.join(app.static_folder, "scripts/"+pipeline[-1]+".py")
-            print file_location, script_location
-            call(["python",script_location,file_location])
-        else:
-            pipeline=[]
-
-
-    elif uform.validate_on_submit():
+    if uform.validate_on_submit():
         if hasattr(uform.image_file.data,'filename'):
-            pipeline = []
+            tuple_list = []
             for file in os.listdir(app.static_folder+"/temp"):
                 os.remove(app.static_folder+"/temp/"+file)
             image = 'temp/' + uform.image_file.data.filename
             uform.image_file.data.save(os.path.join(app.static_folder, image))
 
-    return render_template('index.html', uform=uform, pform=pform, image=image, pipeline = pipeline)
+    return render_template('index.html', uform=uform, pform=pform, image=image, tuple_list = tuple_list, lst=lst)
 
 @app.after_request
 def add_header(response):
