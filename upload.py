5680d068dfe0ae076bcc6cce084f695b47e41547
goelakash <goelakash93@gmail.com>
2016-03-05 14:22:53 +0530

    Pipeline modifies correctly

--- upload.py
+++ upload.py
@@ -1,7 +1,7 @@
 import os
 import time
 import imghdr
-from flask import Flask, render_template, make_response, redirect, url_for
+from flask import Flask, render_template, make_response, redirect, url_for, request, jsonify, json
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 # from flask_pymongo import PyMongo
@@ -13,7 +13,6 @@ app = Flask(__name__)
 # mongo = PyMongo(app)
 app.config['SECRET_KEY'] = 'top secret!'
 bootstrap = Bootstrap(app)
-
 lst=[]
 
 def fill_list():
@@ -23,10 +22,10 @@ def fill_list():
             lst.append((count,file[:-3]))
             count+=1
 
-class PipelineForm(Form):
-    width = TextField('Width')
-    height = TextField('Height')
-    submit = SubmitField('Final Submit')
+# class PipelineForm(Form):
+#     width = TextField('Width')
+#     height = TextField('Height')
+#     submit = SubmitField('Final Submit')
 
 class UploadForm(Form):
     image_file = FileField('Image file')
@@ -37,79 +36,88 @@ tuple_list = []
 
 @app.route('/apply', methods=['GET', 'POST'])
 def apply():
-    if pform.validate_on_submit():
-        global tuple_list,image
-        pipeline_1 = []
-        #get the pipeline operations
-        for a,b in tuple_list:
-            pipeline_1.append(a)
+    uform = UploadForm
+    global tuple_list,image
+    pipeline_1 = []
+    pipeline_2 = []
 
-        val = request.files['file']
-        #conver val to pipeline_2
+    l = request.form.get('arr').split(',')
+    print l, type(l)
+    pipeline_2 = [-1]
+    for _ in l:
+        pipeline_2.append(int(_))
 
+    for a,b in tuple_list:
+        pipeline_1.append(a)
 
-        if pipeline_1 != pipeline_2:
-            i = 0;
-            min_len = min(len(pipeline_1),len(pipeline_2))
-            while i<min_len:
-                if pipeline_1[i]!=pipeline_2[i]:
-                    break
-                i=i+1
+    print pipeline_1
+    print pipeline_2
 
-            if i<len(pipeline_1):
-                tuple_list = tuple_list[:i]
+    if pipeline_1 != pipeline_2:
+        i = 1;
+        min_len = min(len(pipeline_1),len(pipeline_2))
+        while i<min_len:
+            if pipeline_1[i]!=pipeline_2[i]:
+                break
+            i=i+1
 
-            while i<len(pipeline_2):
-                op = lst[pipeline_2[i]]
+        if i<len(pipeline_1):
+            j = i
+            while j<len(pipeline_1):
+                print "Deleting "+tuple_list[j][1]
+                os.remove(tuple_list[j][1])
+                j=j+1
+            tuple_list = tuple_list[:i]
 
-                input_location = os.path.join(app.static_folder, tuple_list[i-1][1])
-                script_location = os.path.join(app.static_folder, "scripts/"+op+".py")
+        while i<len(pipeline_2):
+            op = lst[pipeline_2[i]][1]
+            print op
+            print len(tuple_list)
 
-                print input_location, script_location
+            input_location = os.path.join(app.static_folder, tuple_list[i-1][1])
+            script_location = os.path.join(app.static_folder, "scripts/"+op+".py")
 
-                ext = input_location[input_location.rfind('.')+1:]
-                timestamp = int(time.time())
-                output_location = input_location[:input_location.index('_')+1]+str(timestamp)+"."+ext
+            print "Before: "+input_location
 
-                print output_location
+            name, ext = os.path.splitext(input_location)
+            name = name[:name.rfind("_")]
+            timestamp = time.time()
+            output_location = name+"_"+str(timestamp)+ext
 
-                call(["python",script_location,input_location,output_location])
-                #get image from db
-                #do the operation of pipeline_2[i]
-                #store the image at (image_ID with i) location
+            print "After: "+output_location
 
-                # tuple_list.append((pipeline_2[i]),new_img)
+            call(["python",script_location,input_location,output_location])
 
-                i=i+1
-            image = tuple_list[-1][1]
+            tuple_list.append((pipeline_2[i],output_location))
 
-
-        # if image != None:
-            # file_location = os.path.join(app.static_folder, image)
-            # script_location = os.path.join(app.static_folder, "scripts/"+pipeline[-1]+".py")
-            # print file_location, script_location
-            # call(["python",script_location,file_location])
-        # else:
-        #     pipeline_1=pipeline_2=[]
+            i=i+1
+        print tuple_list[-1][1]
+        image = tuple_list[-1][1]
+    return redirect(url_for("index"))
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global image,tuple_list
-    # pipeline = []
 
     uform = UploadForm()
-    pform = PipelineForm()
+    print "Redirect successful"
 
-    if uform.validate_on_submit():
-        if hasattr(uform.image_file.data,'filename'):
-            tuple_list = []
-            for file in os.listdir(app.static_folder+"/temp"):
-                os.remove(app.static_folder+"/temp/"+file)
-            image = 'temp/' + uform.image_file.data.filename
-            uform.image_file.data.save(os.path.join(app.static_folder, image))
-
-    return render_template('index.html', uform=uform, pform=pform, image=image, tuple_list = tuple_list, lst=lst)
+    try:
+        if uform.validate_on_submit():
+            if hasattr(uform.image_file.data,'filename'):
+                for file in os.listdir(app.static_folder+"/temp"):
+                    os.remove(app.static_folder+"/temp/"+file)
+                fname = uform.image_file.data.filename
+                name, ext = os.path.splitext(fname)
+                image = 'temp/' + name+"_"+str(time.time())+ext
+                tuple_list = [(-1,image)]
+                uform.image_file.data.save(os.path.join(app.static_folder, image))
+                print "No Exceptions"
+    except e:
+        print "Exception happened"
+        print type(e)
+    return render_template('index.html', uform=uform, image=image, lst=lst)
 
 @app.after_request
 def add_header(response):
